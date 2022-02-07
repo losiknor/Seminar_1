eb1c6c199c02c0743004bf8a8ca19f1b335df7b5
Larina <Losiknor@gmail.com>
2022-02-07 13:06:15 +0700

    Задача 02

---
+++ z02/Program.cs
@@ -0,0 +1,13 @@
+﻿// № 02 Даны два числа. Показать большее и меньшее число
+int numberA = new Random().Next(1, 100);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine("Даны два числа   "+ numberA + "   и   " + numberB);
+if (numberA > numberB)
+    Console.WriteLine("Большее число " + numberA + "   Меньшее число " + numberB);
+else 
+{
+    if (numberA < numberB)
+    Console.WriteLine("Большее число " + numberB + "   Меньшее число " + numberA);
+    else
+    Console.WriteLine("Нет большего и меньшего");
+}
