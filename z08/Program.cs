f4a1a5cbcdc4e9b7e9aa0bd9834703ac30a1cf69
Larina <Losiknor@gmail.com>
2022-02-07 14:00:42 +0700

    Задача 08

---
+++ z08/Program.cs
@@ -0,0 +1,7 @@
+﻿// № 08 Показать четные числа от 1 до N
+int numberA = new Random().Next(1, 50);
+Console.WriteLine("Чётные числа от 1 до " + numberA);
+int c = 2;
+while (c <= numberA)
+{Console.WriteLine(c);
+c = c + 2;}
