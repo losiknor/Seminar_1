62b00f0b4cadcebdd7548bea18c45f21258b4257
Larina <Losiknor@gmail.com>
2022-02-07 13:38:10 +0700

    Задача 04

---
+++ z04/Program.cs
@@ -0,0 +1,11 @@
+﻿// № 04 Найти максимальное из трех чисел
+int numberA = new Random().Next(1, 100);
+int numberB = new Random().Next(1, 100);
+int numberС = new Random().Next(1, 100);
+Console.WriteLine("Даны три числа   "+ numberA + "   ;   " + numberB + "   ;   " + numberС);
+int max = numberA;
+if (numberB > max)
+max = numberB;
+if (numberС > max)
+max = numberС;
+Console.WriteLine("Максимальное число   " + max);
